c41a6ef75475b2dbb1455d4ac624f49e4bb1537c
aaguadaa <milenaaaraujo2002@gmail.com>
2023-05-11 18:12:21 -0700

    prueba del git desktop

--- Domain/Model/User.cs
+++ Domain/Model/User.cs
@@ -13,6 +13,7 @@ namespace Domain.Model
         public int Id { get; set; }
         public string Name { get; set; }
         public string APaterno { get; set; }
+        public string AMaterno { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public int? PilotId { get; set; }
