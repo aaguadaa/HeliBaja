721ef27b87329e6be9e43292beded558cc3e634b
aaguadaa <milenaaaraujo2002@gmail.com>
2023-05-21 16:57:45 -0700

    fixed
    #1  Agenda:fixed
    #2  Agenda:fixed
    #3  Agenda:fixed

---
+++ Business/Contracts/IAgendaService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Model;
+using System.Collections.Generic;
+
+namespace Business.Contracts
+{
+    public interface IAgendaService
+    {
+        int CreateAgenda(Agenda agenda);
+        bool UpdateAgenda(Agenda agenda);
+        bool DeleteAgenda(int agendaId);
+        Agenda GetAgendaById(int agendaId);
+        List<Agenda> GetAllAgendas();
+        bool AddFlightToAgenda(int agendaId, int flightId);
+        bool RemoveFlightFromAgenda(int agendaId, int flightId);
+    }
+}
---
+++ Business/Implementation/AgendaService.cs
@@ -0,0 +1,52 @@
+﻿using Business.Contracts;
+using Data.Contracts;
+using Domain.Model;
+using System.Collections.Generic;
+
+namespace Business.Implementation
+{
+    public class AgendaService : IAgendaService
+    {
+        private readonly IAgendaRepository _agendaRepository;
+
+        public AgendaService(IAgendaRepository agendaRepository)
+        {
+            _agendaRepository = agendaRepository;
+        }
+
+        public int CreateAgenda(Agenda agenda)
+        {
+            return _agendaRepository.Add(agenda);
+        }
+
+        public bool UpdateAgenda(Agenda agenda)
+        {
+            return _agendaRepository.Update(agenda);
+        }
+
+        public bool DeleteAgenda(int agendaId)
+        {
+            return _agendaRepository.Delete(agendaId);
+        }
+
+        public Agenda GetAgendaById(int agendaId)
+        {
+            return _agendaRepository.Get(agendaId);
+        }
+
+        public bool AddFlightToAgenda(int agendaId, int flightId)
+        {
+            return _agendaRepository.AddFlight(agendaId, flightId);
+        }
+
+        public bool RemoveFlightFromAgenda(int agendaId, int flightId)
+        {
+            return _agendaRepository.RemoveFlight(agendaId, flightId);
+        }
+
+        public List<Agenda> GetAllAgendas()
+        {
+            return _agendaRepository.GetAdminAgenda();
+        }
+    }
+}
